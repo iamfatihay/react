7a9b40de16587b5a9fe9535a5091b7ad418b21f8
Fatih <16fay61@gmail.com>
2023-05-10 12:24:29 +0200

    react

--- class-notes/DE-03-react-bootstrap/src/App.js
+++ class-notes/DE-03-react-bootstrap/src/App.js
@@ -1,9 +1,16 @@
 import React from "react"
+import MyNavbar from "./components/MyNavbar";
+import "bootstrap/dist/css/bootstrap.min.css";  //! bu satir bir kaliptir, her kullanimda bu lazim
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div >
-      APP
+      <MyNavbar/>
+
+
+
+      <Footer/>
     </div>
   );
 }
