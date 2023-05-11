870ab0a34fdb72c78508aa54b36f1fdd48f2e96c
Fatih <16fay61@gmail.com>
2023-05-11 11:42:09 +0200

    aa

--- class-notes/DE-03-react-sass/src/App.js
+++ class-notes/DE-03-react-sass/src/App.js
@@ -1,9 +1,14 @@
 import React from "react"
+// import './scss/_button.scss'
+// import './scss/_reset.scss'
+// import './scss/_variable.scss'    //bu sekilde de import edilebilir
+import "./App.scss"                  //biz bu sekilde tercih ettik
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div >
-      APP
+      <Header/>
     </div>
   );
 }
---
+++ class-notes/DE-03-react-sass/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <Header>
+        <h1>
+            COMMENTS
+        </h1>
+    </Header>
+  )
+}
+
+export default Header
