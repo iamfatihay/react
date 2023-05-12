476df70fe29fb4f0c3f6da40ac4594db1c9eb246
Fatih <16fay61@gmail.com>
2023-05-12 10:04:01 +0200

    aa

--- DE-01-Starter/src/App.js
+++ DE-01-Starter/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div >
       <h1>Welcome React Sass</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt obcaecati aspernatur similique autem veritatis, voluptatum inventore harum dolores id tenetur.</p>
+      <Header/>
     </div>
   );
 }
