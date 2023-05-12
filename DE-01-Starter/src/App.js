626b28e09a192298482b17f230219ed7651753aa
Fatih <16fay61@gmail.com>
2023-05-12 09:39:17 +0200

    aa

---
+++ DE-01-Starter/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div >
+      <h1>Welcome React Sass</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt obcaecati aspernatur similique autem veritatis, voluptatum inventore harum dolores id tenetur.</p>
+    </div>
+  );
+}
+
+export default App;
