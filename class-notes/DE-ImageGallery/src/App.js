3c9d99f679cf34c7691fe0d3290cdab502b1eff8
Fatih <16fay61@gmail.com>
2023-05-14 01:42:36 +0200

    aa

---
+++ class-notes/DE-ImageGallery/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Picture from "./components/Picture";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Image Gallery</h1>
+      <div className="container">
+        <Picture/>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ class-notes/DE-ImageGallery/src/components/Picture.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/style.css";
+import {data} from "../helper/data";
+
+const Picture = () => {
+    console.log(data);
+  return (
+    <div>Picture</div>
+  )
+}
+
+export default Picture;
