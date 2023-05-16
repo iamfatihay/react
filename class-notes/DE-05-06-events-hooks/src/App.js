97fca6a0f6476ce8f86c5505f052b1605f9595cf
Fatih <16fay61@gmail.com>
2023-05-16 09:35:42 +0200

    aa

--- class-notes/DE-05-06-events-hooks/src/App.js
+++ class-notes/DE-05-06-events-hooks/src/App.js
@@ -3,14 +3,16 @@ import Events from "./1-events-hooksIntro/Events";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Hooks from "./2-hooks/Hooks";
 import Counter from "./2-classComponents/Counter";
+import Form from "./3-Forms/form";
 
 function App() {
   return (
     <div >
       {/* <Events/> */}
       {/* <Hooks/> */}
-      <Counter/>
-      <Counter count={20} />
+      {/* <Counter/>
+      <Counter count={20} /> */}
+      <Form/>
 
 
     </div>
