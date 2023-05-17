b4ab7d481ba458219795ea1d7ff17c91576f9e4c
Fatih <16fay61@gmail.com>
2023-05-17 12:05:35 +0200

    aaa

--- class-notes/DE-07-NBA-legends/src/components/PlayerCard.jsx
+++ class-notes/DE-07-NBA-legends/src/components/PlayerCard.jsx
@@ -1,14 +1,27 @@
 import Card from "react-bootstrap/Card";
+import { useState } from "react";
 
-const PlayerCard = ({img,name,statistics}) => {
-  return (
-    <Card className="rounded-2 m-auto player-card" role="button">
-      <Card.Img variant="top" src={img} className="player-logo" />
-      <Card.Footer>
-        <Card.Title>{name}</Card.Title>
-      </Card.Footer>
-    </Card>
-  )
+const PlayerCard = ({ img, name, statistics }) => {
+    //? onClick={() => setshowImage(!showImage)}    bu sekilde de olur
+    const [showImage, setshowImage] = useState(true)
+    const handleClick = () => setshowImage(!showImage);
+
+    return (
+        <Card className="rounded-2 m-auto player-card" role="button" onClick={handleClick}>
+            {showImage ? (<Card.Img variant="top" src={img} className="player-logo" />)
+                : (
+                    <ul className="m-auto">
+                        {statistics.map((item, i) => {
+                            return (
+                                <li key={i} className="h5 text-start list-unstyled">🏀 {item} </li>
+                            )
+                        })}
+                    </ul>)}
+            <Card.Footer>
+                <Card.Title>{name}</Card.Title>
+            </Card.Footer>
+        </Card>
+    )
 }
 
 export default PlayerCard
