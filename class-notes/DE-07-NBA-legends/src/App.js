19321c5beba6af3d3e73f7aecf30949480468c74
Fatih <16fay61@gmail.com>
2023-05-17 10:07:04 +0200

    aaa

---
+++ class-notes/DE-07-NBA-legends/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Container className="text-center mt-4">
+      <Header/>
+
+
+    </Container>
+  );
+}
+
+export default App;
