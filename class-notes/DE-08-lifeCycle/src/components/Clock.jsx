da95b40dc9d1d1214199b1bfee11101683306a4c
Fatih <16fay61@gmail.com>
2023-05-22 09:14:58 +0200

    aa

--- class-notes/DE-08-lifeCycle/src/components/Clock.jsx
+++ class-notes/DE-08-lifeCycle/src/components/Clock.jsx
@@ -1,40 +1,37 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import moment from "moment"
 
 const Clock = () => {
-const [zaman, setZaman] = useState(moment());
+    const [zaman, setZaman] = useState(moment());
 
 
-useEffect(()=>{
+    useEffect(() => {
+        const time = setInterval(() => {
+            setZaman(moment());
+            console.log("merhaba");
+        }, 1000);
+
+        return () => {
+            clearInterval(time)
+            console.log("başka sayfaya gidildiği için interval durdu");
+        }
 
 
- const time=  setInterval(() => {
-  setZaman(moment());
-  console.log("merhaba");
-}, 1000);
-
-return()=>{
-clearInterval(time)
-console.log("başka sayfaya gidildiği için interval durdu");
-}
-
-
-
-//!sayfa ilk render edildiğinde setInterval başlasın
-},[])
+        //!sayfa ilk render edildiğinde setInterval başlasın
+    }, [])
 
 
 
 
-  return (
-    <div>
-      {zaman.format("HH")}
-      {zaman.format("ss") % 2 === 0 ? ":" : " "}
-      {zaman.format("mm")}
-      {zaman.format("ss") % 2 === 0 ? ":" : " "}
-      {zaman.format("ss")}
-    </div>
-  );
+    return (
+        <div>
+            {zaman.format("HH")}
+            {zaman.format("ss") % 2 === 0 ? ":" : " "}
+            {zaman.format("mm")}
+            {zaman.format("ss") % 2 === 0 ? ":" : " "}
+            {zaman.format("ss")}
+        </div>
+    );
 }
 
 export default Clock
