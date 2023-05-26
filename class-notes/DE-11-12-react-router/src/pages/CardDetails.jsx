445c79b3567b3472392cbddb5419d40d9b1b84e4
Fatih <16fay61@gmail.com>
2023-05-26 11:58:47 +0200

    aa

--- class-notes/DE-11-12-react-router/src/pages/CardDetails.jsx
+++ class-notes/DE-11-12-react-router/src/pages/CardDetails.jsx
@@ -2,24 +2,31 @@ import React from 'react';
 
 import data from "../data";
 import { useAccordionButton } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const CardDetails = () => {
-  
-  const {namee}=useParams() 
+  const navigate=useNavigate()
+  const { namee } = useParams()
   // console.log(namee);
 
   return (
     <div className="container text-center mt-4">
-      {data.map((i)=>i.name===namee && ( <div>
-          <h3>{i.name} </h3>
-          <img src={i.img} alt="#" width="300px" />
-          <h3>{i.text} </h3>
-          <h4>{i.yorum} </h4>
+      {data.map((i) => i.name === namee && (<div>
+        <h3>{i.name} </h3>
+        <img src={i.img} alt="#" width="300px" />
+        <h3>{i.text} </h3>
+        <h4>{i.yorum} </h4>
+        <div>
+          <button className="btn btn-primary" onClick={() => navigate(-1)} >
+            GO BACK
+          </button>
+          <button className="btn btn-warning" onClick={() => navigate("/")} >
+            GO HOME
+          </button>
+        </div>
 
 
-
-      </div> ))}
+      </div>))}
     </div>
   );
 }
--- class-notes/DE-11-12-react-router/src/pages/TeacherDetails.jsx
+++ class-notes/DE-11-12-react-router/src/pages/TeacherDetails.jsx
@@ -1,41 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 
 const TeacherDetails = () => {
-  // id yi suslu icinde yazarak destructuring yaptik, suslu disinda yazarsak "id.id" olarak degere ulasabilirdik
+  //? id yi suslu icinde yazarak destructuring yaptik, suslu disinda yazarsak "id.id" olarak degere ulasabilirdik
   const {id}=useParams()
   // console.log(id);
-
-  const[kisi,setKisi]=useState()
+  const[kisi,setKisi]=useState([])
+  const navigate=useNavigate()
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => res.json())
       .then((data) => setKisi(data))
       .catch((err) => console.log(err));
-  }, []);
+  }, [id]);
 //  console.log(kisi);
+//! useEffect de ikinci parametredeki array i bos birakinca bir defa render ediliyor,
+//! ama icinde bir deger eklendiginde o deger her degistiginde tekrar render ediliyor.
+//! navigate(-sayi) ile sayi kac ise o kadar sekme geri doner, yani (-1) bir onceki sayfaya doner
 
   return (
     <Container className="text-center mt-4">
       <div>
         <h3> </h3>
         <img
-       
-          alt=""
+          src={`https://avatars.dicebear.com/api/avataaars/${id}.svg`}
+          alt="#"
           width="250px"
         />
-        <h4>{}</h4>
-        <h5>{}</h5>
-        <h5>{}</h5>
-        <h5>{}</h5>
+        <h4>{kisi.username}</h4>
+        <h5>{kisi.name}</h5>
+        <h5>{kisi.phone}</h5>
+        <h5>{kisi.website}</h5>
+        <h5>{kisi.email}</h5>
         <div>
-          <button className="btn btn-primary" >
+          <button className="btn btn-primary" onClick={()=>navigate(-1)} >
             GO BACK
           </button>
-          <button className="btn btn-warning" >
+          <button className="btn btn-warning" onClick={()=>navigate("/")} >
             GO HOME
           </button>
         </div>
