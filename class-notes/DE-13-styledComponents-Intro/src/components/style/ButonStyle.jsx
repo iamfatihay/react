35ceec40a1226f7db6ae30450276253c3e81d525
Fatih <16fay61@gmail.com>
2023-05-30 10:04:14 +0200

    aa

---
+++ class-notes/DE-13-styledComponents-Intro/src/components/style/ButonStyle.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const ButtonS = styled.button`
+  background-color: orange;
+  color: blue;
+  cursor: pointer;
+  padding: 1rem 1.5rem; 
+  border-radius: 5px;
+  font-family: "tahoma";
+  font-size: 30px;
+  &:hover {
+    transform: scale(0.95);
+  }
+`;
+
+export const DomatesButon=styled(ButtonS)`
+    color: green;
+    background-color: aquamarine;
+    border: 5px solid red;
+    width: 300px;
+`;
+
+export default ButtonS;
