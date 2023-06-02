0a35ae6a7c0218ad2b24ff6f91f23e7a4c5c6f64
Fatih <16fay61@gmail.com>
2023-06-02 11:23:46 +0200

    aa

--- class-notes/DE-14-15-RecipeAppProject/src/router/PrivateRouter.jsx
+++ class-notes/DE-14-15-RecipeAppProject/src/router/PrivateRouter.jsx
@@ -5,7 +5,9 @@ const PrivateRouter = () => {
   return (
     (localStorage.getItem("username")==="ipek" && localStorage.getItem("password")==="1234" )
      ? (<Outlet/>) : (<Navigate to="/" />)
-  )
+  );
+  // kullanici doğru şifre ve isim girdiyse Outlet komponenti sayesinde istenilen sayfaya git,
+  // yanlışlık varsa Navigate ile bulunduğun yerde (Login) kal ya da başka path veriledebilir
 }
 
 export default PrivateRouter
