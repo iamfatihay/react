af10efcbaf2d9d865cac5d44312f2a21ce52c8f4
Fatih <16fay61@gmail.com>
2023-06-05 12:44:29 +0200

    aa

--- class-notes/DE-16-Context-Reducer/reducer/src/UseStateExample.js
+++ class-notes/DE-16-Context-Reducer/reducer/src/UseStateExample.js
@@ -3,15 +3,23 @@ import load from "./img/loading-loading-gif.gif";
 
 const UseStateExample = () => {
  
+  const [dogResim,setDog]=useState(""); 
+  const [loading,setLoading]=useState(false) 
+  const [error,setError]=useState("")
 
   const fetchDog = () => {
+
+    setLoading(true)
+
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((res) => res.json())
       .then((data) => {
-     console.log(data)
+        setDog(data.message)
+        setLoading(false)
       })
       .catch(() => {
-     
+        setError("DIKKAT URL DE HATA VAR")
+        setLoading(false)
       });
   };
   return (
@@ -22,12 +30,16 @@ const UseStateExample = () => {
       }}
     >
       <button
-        disabled
+        onClick={fetchDog}
+        disabled={loading}
         style={{ width: "200px", margin: "1rem" }}
       >
         Fetch Dog
       </button>
-    
+      <img src={dogResim} alt="" />
+      {loading && <img src={load} alt="" />}
+      {/* {dogResim && <img src={dogResim} alt="" />} */}
+      {error && <h2>{error}</h2> }
     </div>
   );
 };
