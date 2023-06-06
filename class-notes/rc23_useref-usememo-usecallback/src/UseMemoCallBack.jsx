7c7876c8095a1707c0649743ac21cc453488d468
Fatih <16fay61@gmail.com>
2023-06-06 12:08:46 +0200

    a

--- class-notes/rc23_useref-usememo-usecallback/src/UseMemoCallBack.jsx
+++ class-notes/rc23_useref-usememo-usecallback/src/UseMemoCallBack.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import axios from "axios";
 import Users from './components/Users';
 
 const UseMemoCallBack = () => {
   const[kisiler,setKisiler]=useState([])
   const[text,setText]=useState("")
+  const[bitmisHali,setBitmis]=useState("")
 
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>setKisiler(res.data))
   }, [])
   
-
+  const filtreli = useMemo(()=>kisiler.filter((i)=>i.name.includes(bitmisHali)),[kisiler,bitmisHali]);
 
 
   return (
     <div>
       <input type="search" onChange={(e)=>setText(e.target.value)} />
-      <button>SEARCH</button>
-      <Users kisiler={kisiler} />
+      <button onClick={()=>setBitmis(text)} >SEARCH</button>
+      {/* <Users kisiler={kisiler} /> */}
+      <Users kisiler={filtreli} />
     </div>
   )
 }
