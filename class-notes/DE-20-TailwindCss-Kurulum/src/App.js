f1180a2680b062edf047e5f66f3a2cd155f58649
Fatih <16fay61@gmail.com>
2023-06-07 10:02:02 +0200

    aaa

---
+++ class-notes/DE-20-TailwindCss-Kurulum/src/App.js
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+function App() {
+
+  return (
+    <div className="bg-fuchsia-300 h-60" >
+      <h1 className="text-3xl font-bold underline" >Hello</h1>
+      {/* <Card /> */}
+    </div>
+  );
+}
+
+export default App;
