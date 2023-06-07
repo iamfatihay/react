a6777ef8be2b0476f1cc28cfa1a4430cbb5aa07a
Fatih <16fay61@gmail.com>
2023-06-07 11:06:48 +0200

    a

--- class-notes/DE-20-TailwindCss-Kurulum/src/App.js
+++ class-notes/DE-20-TailwindCss-Kurulum/src/App.js
@@ -1,11 +1,21 @@
-import Card from "./Card";
 
 function App() {
 
   return (
-    <div className="bg-fuchsia-300 h-60" >
-      <h1 className="text-3xl font-bold underline" >Hello</h1>
-      {/* <Card /> */}
+    <div className="main" >
+      {/* <h1 className="text-3xl font-bold underline" >Hello</h1> */}
+      <div className="card">
+        <h2>Title</h2>
+        <img className="h-[250px] rounded-xl"  src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg" alt="" />
+        <p className="text-white line-clamp-1 hover:line-clamp-none" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis autem debitis beatae ex sit quasi ea. Asperiores, numquam? Id odio nesciunt illo! Voluptatibus perspiciatis ullam placeat delectus, accusamus quasi reprehenderit. </p>
+      </div>
+      <div className="card">
+        <h2>Title</h2>
+        <img className="h-[250px] rounded-xl"  src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg" alt="" />
+        <p className="text-white line-clamp-1 hover:line-clamp-none" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis autem debitis beatae ex sit quasi ea. Asperiores, numquam? Id odio nesciunt illo! Voluptatibus perspiciatis ullam placeat delectus, accusamus quasi reprehenderit. </p>
+      </div>
+      <h1>Deneme</h1>
+      <h2>Deneme</h2>
     </div>
   );
 }
