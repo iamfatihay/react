28c8fdf41b8be7173818f82ca240e086b400a159
Fatih <16fay61@gmail.com>
2023-06-16 00:39:54 +0200

    aa

--- class-notes/Portfolio/src/components/home/Social.jsx
+++ class-notes/Portfolio/src/components/home/Social.jsx
@@ -3,15 +3,17 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="" className="home__social-icon" target="_blank">
-        <i class="uil uil-instagram"></i>
-        </a>
-        <a href="" className="home__social-icon" target="_blank">
-        <i class="uil uil-github"></i>   
-        </a>
-        <a href="" className="home__social-icon" target="_blank">
+      <a href="https://www.linkedin.com/in/fatih-ay1661/" className="home__social-icon" target="_blank">
         <i class="uil uil-linkedin-alt"></i>
-        </a>
+      </a>
+      <a href="https://github.com/iamfatihay" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+      </a>
+      <a href="https://twitter.com/Fatih__AY" className="home__social-icon" target="_blank">
+        <i class="uil uil-twitter-alt"></i>
+      </a>
+
+
     </div>
   )
 }
