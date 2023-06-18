0e34b62c1002feb9a91405d57fa667fe2415cace
Fatih <16fay61@gmail.com>
2023-06-18 17:14:35 +0200

    aa

--- class-notes/Portfolio/src/App.js
+++ class-notes/Portfolio/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Qualification from "./components/qualification/Qualification";
+import Work from "./components/work/Work";
 
 
 function App() {
@@ -12,10 +13,11 @@ function App() {
     <>
       <Header />
       <main className="main">
-        <Home />
-        <About/>
-        <Skills/>
-        <Qualification/>
+        {/* <Home /> */}
+        {/* <About/> */}
+        {/* <Skills/> */}
+        {/* <Qualification/> */}
+        <Work/>
       </main>
     </>
   );
