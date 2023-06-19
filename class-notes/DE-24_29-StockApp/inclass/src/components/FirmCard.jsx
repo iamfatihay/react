955afb82016bf34269d79ba50db281419431069f
Fatih <16fay61@gmail.com>
2023-06-19 12:21:29 +0200

    aa

---
+++ class-notes/DE-24_29-StockApp/inclass/src/components/FirmCard.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
+
+export default function FirmCard({ firm }) {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {firm?.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {firm?.address}
+                </Typography>
+            </CardContent>
+            <CardMedia
+                component="img"
+                sx={{ height: 130, objectFit: "contain" }}
+                image={firm?.image}
+                title={firm?.name}
+                alt={firm?.name}
+            />
+            <CardContent>
+
+                <Typography variant="body2" color="text.secondary">
+                    Phone: {firm?.phone}
+                </Typography>
+            </CardContent>
+
+            <CardActions sx={{
+                display: "flex",
+                justifyContent: "center", alignItems: "center", gap: 2
+            }} >
+                <EditIcon sx={{ cursor: "pointer", "&:hover": { color: "red" } }} />
+                <DeleteOutlineIcon />
+            </CardActions>
+        </Card>
+    );
+}
--- class-notes/DE-24_29-StockApp/inclass/src/pages/Firms.jsx
+++ class-notes/DE-24_29-StockApp/inclass/src/pages/Firms.jsx
@@ -3,12 +3,17 @@
 // import axios from "axios";
 import { useEffect } from "react";
 import useStockCall from "../hooks/useStockCall";
+import Typography from "@mui/material/Typography";
+import { Button, Grid } from "@mui/material";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
 
 const Firms = () => {
   // const dispatch = useDispatch();
   // const { token } = useSelector(state => state.auth);
 
   const {getStockData} = useStockCall()
+  const {firms}=useSelector(state=>state.stock)
 
   //? firms verileri bana birden fazla yerde lazım olduğu için fonksiyonu burada değil de her yerden erişebileceğim bir noktada tanımlıyorum. İçerisinde react hookları lazım olduğu için de bu ortak nokta en iyi custom hook olmuş oluyor.
   // const getFirms = async () => {
@@ -32,7 +37,21 @@ const Firms = () => {
     getStockData("firms");
   }, []);
 
-  return <div>Firms</div>;
+  return <div>
+    <Typography variant="h4" color="error" mb={3}>
+      Firms
+    </Typography>
+    <Button variant="contained">New Firm</Button>
+    <Grid container>
+      {
+        firms?.map(firm=>(
+          <Grid item key={firm.id} >
+            <FirmCard firm={firm} />
+          </Grid>
+        ))
+      }
+    </Grid>
+  </div>;
 }
 
 export default Firms
