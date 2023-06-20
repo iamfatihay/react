f9ff693329f0f7dfef26105285ee0bedacfbbb3c
Fatih <16fay61@gmail.com>
2023-06-20 10:16:43 +0200

    aa

---
+++ class-notes/DE-24_29-StockApp/inclass/src/components/FirmModal.jsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { TextField } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function FirmModal({open,handleClose}) {
+//   const [open, setOpen] = React.useState(false);
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
+//   lifting state up
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <TextField
+            label="User Name"
+            name="username"
+            id="userName"
+            type="text"
+            variant="outlined"
+          />
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- class-notes/DE-24_29-StockApp/inclass/src/pages/Firms.jsx
+++ class-notes/DE-24_29-StockApp/inclass/src/pages/Firms.jsx
@@ -1,12 +1,13 @@
 // import {useDispatch, useSelector} from "react-redux";
 // import { fetchFail, fetchStart, getSucces } from "../features/stockSlice";
 // import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useStockCall from "../hooks/useStockCall";
 import Typography from "@mui/material/Typography"
 import { Button, Grid } from "@mui/material";
 import { useSelector } from "react-redux";
 import FirmCard from "../components/FirmCard";
+import FirmModal from "../components/FirmModal";
 
 const Firms = () => {
   // const dispatch = useDispatch();
@@ -14,6 +15,9 @@ const Firms = () => {
 
   const { getStockData } = useStockCall();
   const {firms} = useSelector(state=> state.stock)
+  const [open, setOpen] =useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   //? firms verileri bana birden fazla yerde lazım olduğu için fonksiyonu burada değil de her yerden erişebileceğim bir noktada tanımlıyorum. İçerisinde react hookları lazım olduğu için de bu ortak nokta en iyi custom hook olmuş oluyor.
   // const getFirms = async () => {
@@ -42,13 +46,14 @@ const Firms = () => {
     <Typography variant="h4" color="error" mb={3}>
       Firms
     </Typography>
-    <Button variant="contained">New Firm</Button>
+    <Button variant="contained" onClick={handleOpen}>New Firm</Button>
+    <FirmModal open={open} handleClose={handleClose} />
     <Grid container sx={{
       display:"flex",
       justifyContent:"center",
-      alignItems:"center",
+      alignItems: "center",
       gap:2
-    }} >
+    }}>
       { 
         firms?.map(firm =>(
           <Grid item key={firm.id}>
