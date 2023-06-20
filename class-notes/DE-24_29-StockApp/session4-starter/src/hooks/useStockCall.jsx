74b363caebeb4e0720c64b50ac7dbe057b80fa49
Fatih <16fay61@gmail.com>
2023-06-20 09:34:52 +0200

    hgfd

--- class-notes/DE-24_29-StockApp/session4-starter/src/hooks/useStockCall.jsx
+++ class-notes/DE-24_29-StockApp/session4-starter/src/hooks/useStockCall.jsx
@@ -3,12 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchFail, fetchStart, getSucces } from "../features/stockSlice";
 import axios from "axios";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
-import useAxios from "./useAxios";
 
 const useStockCall = () => {
   const dispatch = useDispatch();
   const { token } = useSelector(state => state.auth);
-  const { axiosWithToken } = useAxios();
 
   //   const getFirms = async () => {
   //     const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -44,38 +42,12 @@ const useStockCall = () => {
   //   };
   //! yukarıdaki gib her seferinde yazmak yerine daha modüler bir yapı kurarak tek bir fonksiyonla bir den fazla get işlemini gerçekleştirebiliyoruz
   const BASE_URL = process.env.REACT_APP_BASE_URL;
-  // const getStockData = async (url) => {
-  //   dispatch(fetchStart());
-  //   try {
-  //     const { data } = await axios.get(`${BASE_URL}stock/${url}/`, {
-  //       headers: { Authorization: `Token ${token}` },
-  //     });
-  //     console.log(data);
-  //     dispatch(getSucces({ data, url }));
-  //   } catch (error) {
-  //     dispatch(fetchFail());
-  //   }
-  // };
-
-  // const deleteStockData = async (url,id) => {
-  //   dispatch(fetchStart());
-  //   try {
-  //      await axios.delete(`${BASE_URL}stock/${url}/${id}/`, {
-  //       headers: { Authorization: `Token ${token}` },
-  //     });
-  //     getStockData(url);
-  //     toastSuccessNotify(`${url} successfuly deleted!`)
-  //   } catch (error) {
-  //     dispatch(fetchFail());
-  //     toastErrorNotify(`${url} not successfuly deleted!`);
-
-  //   }
-  // };
-  //! istek atarken ortak olan base_url  ve token gibi değerleri her seferinde yazmak yerine axios instance kullanarak bunları orada tanımlıyoruz. Ve sonrasında sadece istek atmak istediğimiz end pointi yazmamız yeterli oluyor.
-  const getStockData = async url => {
+  const getStockData = async (url) => {
     dispatch(fetchStart());
     try {
-      const { data } = await axiosWithToken.get(`stock/${url}/`);
+      const { data } = await axios.get(`${BASE_URL}stock/${url}/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
       console.log(data);
       dispatch(getSucces({ data, url }));
     } catch (error) {
@@ -83,15 +55,18 @@ const useStockCall = () => {
     }
   };
 
-  const deleteStockData = async (url, id) => {
+  const deleteStockData = async (url,id) => {
     dispatch(fetchStart());
     try {
-      await axiosWithToken.delete(`stock/${url}/${id}/`);
+       await axios.delete(`${BASE_URL}stock/${url}/${id}/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
       getStockData(url);
-      toastSuccessNotify(`${url} successfuly deleted!`);
+      toastSuccessNotify(`${url} successfuly deleted!`)
     } catch (error) {
       dispatch(fetchFail());
       toastErrorNotify(`${url} not successfuly deleted!`);
+
     }
   };
 
