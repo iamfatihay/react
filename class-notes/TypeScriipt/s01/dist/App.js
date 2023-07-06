9ed87cbeff62611e5f69f3e24dae958e9a8a6fe7
Fatih <16fay61@gmail.com>
2023-07-06 15:58:00 +0200

    aa

---
+++ class-notes/TypeScriipt/s01/dist/App.js
@@ -0,0 +1,23 @@
+"use strict";
+// console.log('Hello World')
+// const button = document.getElementById('submit');
+// const a = document.getElementById('input1')
+// const b = document.getElementById('input2')
+// const result = document.getElementById('result');
+// function add(a , b) {
+//     return a + b;
+// }
+// button.addEventListener('click', function () {
+//     result.innerText = `${add(a.value, b.value)}`;
+// })
+//This is a comment
+
+//ES5 ile uygun compile
+// const x = 5;
+// let y = 6;
+
+// const carp = (c, d) => {
+//     c * d;
+// };
+
+
