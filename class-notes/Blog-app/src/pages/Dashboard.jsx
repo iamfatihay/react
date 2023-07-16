1c1d173735e0e372b44e6eacbc997137e4cf467e
Fatih <16fay61@gmail.com>
2023-07-16 14:09:57 +0200

    a

--- class-notes/Blog-app/src/pages/Dashboard.jsx
+++ class-notes/Blog-app/src/pages/Dashboard.jsx
@@ -34,6 +34,7 @@ const Dashboard = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
