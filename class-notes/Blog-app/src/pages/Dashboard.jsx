0efe22b457f468b51a27dff961269d35419e9c08
Fatih <16fay61@gmail.com>
2023-07-16 14:08:58 +0200

    aa

--- class-notes/Blog-app/src/pages/Dashboard.jsx
+++ class-notes/Blog-app/src/pages/Dashboard.jsx
@@ -1,21 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useSelector } from "react-redux";
 import useBlogCalls from "../hooks/useBlogCalls";
 import { Grid } from '@mui/material';
 import { flexCenter } from "../styles/globalStyle";
 import BlogCard from '../components/blog/BlogCard';
 import { Helmet } from "react-helmet";
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 
 const Dashboard = () => {
   const { getBlogDataPublic } = useBlogCalls();
   const { blogs } = useSelector(state => state.blog);
+  const [scrollToTop, setScrollToTop] = useState(false);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   useEffect(() => {
-    getBlogDataPublic("blogs");
+    getBlogDataPublic('blogs');
     console.log(blogs);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setScrollToTop(true);
+      } else {
+        setScrollToTop(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
+
   return (
     <div>
       <Grid container sx={flexCenter}>
@@ -28,6 +48,22 @@ const Dashboard = () => {
           </Grid>
         ))}
       </Grid>
+      {scrollToTop && (
+        <button
+          onClick={handleScrollToTop}
+          style={{
+            position: 'fixed',
+            bottom: '15px',
+            right: '10px',
+            padding: '7px',
+            fontSize: '16px',
+            backgroundColor:"lightgray",
+            borderRadius:"5px",
+          }}
+        >
+          <ArrowUpwardIcon/>
+        </button>
+      )}
     </div>
   )
 }
