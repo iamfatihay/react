86a8beee49752c9bde5d490183929803de5b144a
Fatih <16fay61@gmail.com>
2023-07-30 21:06:51 +0200

    aa

--- class-notes/DE-14-15-RecipeAppProject/src/pages/register/register.jsx
+++ class-notes/DE-14-15-RecipeAppProject/src/pages/register/register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
   return (
     <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
       <div
-        className={`mt-[3vh] mx-auto overflow-hidden relative w-[480px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
+        className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
       >
         <form
           className="absolute inset-[2px] rounded-[8px] bg-gray-100 dark:bg-[#28292d] z-[10] flex flex-col py-[50px] px-[40px]"
