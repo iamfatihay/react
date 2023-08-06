2feeb9a61e97110ba2f9e9ffa82b659790309c82
Fatih <16fay61@gmail.com>
2023-08-06 12:33:42 +0200

    aa

--- class-notes/DE-Router-example/src/pages/notFound/NotFound.jsx
+++ class-notes/DE-Router-example/src/pages/notFound/NotFound.jsx
@@ -7,9 +7,9 @@ const NotFound = () => {
   const navigate=useNavigate()
 
   return (
-    <Container className='d-flex justify-content-center align-items-center mt-5'>
-      <img src={resim} alt="" width="400px" />
-      <button className="btn btn-danger" onClick={()=>navigate("/")} > HOME </button>
+    <Container className='d-flex flex-column justify-content-center align-items-center mt-5'>
+      <img className='mt-5' src={resim} alt="img" width="600px"/>
+      <button className="btn btn-primary" onClick={()=>navigate("/")} > HOME </button>
     </Container>
   )
 }
